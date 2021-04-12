11f52ebec82abc3bbbc730f43ee6d95e7fa6f489
york17 <317860079@qq.com>
2021-04-12 18:14:30 +0800

    1、发布webpack4书籍；

--- demo_code/src/js/lazyload.js
+++
@@ -1,5 +0,0 @@
-console.log('lazyload文件被加载了');
-
-export function print() {
-    console.log('我是lazyload文件');
-}
--- demo_code/src/js/other_entrance.js
+++
@@ -1,9 +0,0 @@
-console.log('我是另外一个入口，哈哈哈哈！！！');
-
-// import $ from './jquery'
-// console.log($);
-
-export function print(name = 'webpack') {
-  console.log(name);
-}
-
